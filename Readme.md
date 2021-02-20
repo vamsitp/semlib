46b20c91b5b8ac696a1565dc8cf2236cacddfa4b
Vamsi TP <vamsitp@microsoft.com>
2021-02-20 13:01:42 +0530

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Semantic library to extract Keywords from text - based on Tyler Jensen's code/blog
+
+**Credit**: https://www.tsjensen.com/blog/post/2010/03/14/Keyword+Extraction+In+C+With+Word+Cooccurrence+Algorithm
---
+++ SemLib.Tests/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace SemLib.Tests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.IO;
+	using SemLib;
+
+    public class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				//Note: you will have to supply your own text files
+				Console.Write("text-1: ");
+				var t1 = Console.ReadLine();
+
+				Console.Write("text-2: ");
+				var t2 = Console.ReadLine();
+
+				var ka = new KeywordAnalyzer();
+
+				var t1Analysis = ka.Analyze(t1);
+				var t2Analysis = ka.Analyze(t2);
+
+				Console.WriteLine("text-1");
+				foreach (var key in t1Analysis.Keywords)
+				{
+					Console.WriteLine("   key: {0}, rank: {1}", key.Word, key.Rank);
+				}
+
+				Console.WriteLine("text-2");
+				foreach (var key in t2Analysis.Keywords)
+				{
+					Console.WriteLine("   key: {0}, rank: {1}", key.Word, key.Rank);
+				}
+
+				Console.WriteLine("text-1");
+				var t1Keywords = (from n in t1Analysis.Keywords select n);
+				foreach (var key in t1Keywords)
+				{
+					Console.WriteLine("   {0}", key.Word);
+				}
+
+				Console.WriteLine("text-2");
+				var t2Keywords = (from n in t2Analysis.Keywords select n);
+				foreach (var key in t2Keywords)
+				{
+					Console.WriteLine("   {0}", key.Word);
+				}
+
+				Console.WriteLine("--------------------------------------------------");
+			}
+		}
+	}
+}
